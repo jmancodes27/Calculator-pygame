96c5b085f1d2666d59607002c737c86f32f10574
fried <fried@DERBERT>
2024-03-02 21:47:16 -0800

    added system for mouse clicking

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -17,7 +17,11 @@ dot = pygame.image.load("Dot.png")
 calculator = pygame.image.load("calc_gui.png")
 calculator = pygame.transform.scale(calculator, (int(calculator.get_width() * 0.75), int(calculator.get_height() * 0.75)))
 cur_num_disp = [one, two, three, four, five, six, dot, seven, eight, nine, zero, one]
-
+mouse_pos_x = 0
+mouse_pos_y = 0
+mouse_column = 0
+mouse_row = None
+num1 = "0"
 pygame.init()
 running = True
 
@@ -26,24 +30,29 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    print("working")
-
+    mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+    for i in range(5):
+        if mouse_pos_x >= (i) * 125 and mouse_pos_x < (i + 1) * 125:
+            mouse_column = i
+            break
+    for i in range(6):
+        if mouse_pos_y >= 170:
+            if mouse_pos_y >= i * 71 + 170 and mouse_pos_y <= (i + 1) * 71 + 170:
+                mouse_row = i
+                break
+        else:
+            break
+    if pygame.mouse.get_pressed()[0] and not mouse_was_pressed:
+        if mouse_column == 0:
+            if mouse_row == 2:
+                num1 += "7"
+                mouse_was_pressed = True
+    if not pygame.mouse.get_pressed()[0]:
+        mouse_was_pressed = False
+    print(num1)
     screen.fill((0, 0, 0)) 
     screen.blit(calculator, (0, 0))
     for i in range(12):
         screen.blit(cur_num_disp[i], (-200 + i * 30, -135))
-    
-    #screen.blit(zero, (-200, 0))
-    #screen.blit(one, (-150, 0))
-    #screen.blit(two, (-100, 0))
-    #screen.blit(three, (-50, 0))
-    #screen.blit(four, (0, 0))
-    #screen.blit(five, (50, 0))
-    #screen.blit(six, (100, 0))
-    #screen.blit(seven, (150, 0))
-    #screen.blit(eight, (200, 0))
-    #screen.blit(nine, (250, 0))
-    #screen.blit(dot, (270, 0))
 
     pygame.display.flip()
